1a44abec97dacf341eb02b8101ce3b9fa84c9c8c
Tom Fletcher <fletcher.tom@gmail.com>
2018-01-22 12:55:38 +0000

    Add convenience script for keeping my project up to date by copying this repo over to it (Symlinks don't work on Windows)

---
+++ copyLibraryToUnityProject.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp -r Assets/UnityARInterface ../whats-on-the-horizon/Assets/
+cp -r Assets/UnityARKitPlugin ../whats-on-the-horizon/Assets/
+cp -r Assets/GoogleARCore ../whats-on-the-horizon/Assets/
+
