bd966237c610cc0518d71c36189be01134c82cb3
Tom Fletcher <fletcher.tom@gmail.com>
2018-01-30 17:22:52 +0000

    Add utility script for copying changes made in project to here.

---
+++ copyFromUnityProjectToHere.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp -r ../whats-on-the-horizon/Assets/UnityARInterface Assets/
+cp -r ../whats-on-the-horizon/Assets/UnityARKitPlugin Assets/
+cp -r ../whats-on-the-horizon/Assets/GoogleARCore Assets/
+
