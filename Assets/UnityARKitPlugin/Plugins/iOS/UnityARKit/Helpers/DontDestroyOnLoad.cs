198b7b8a5c90ee97ad6eba5e07996d318634bdad
Tom Fletcher <fletcher.tom@gmail.com>
2018-02-12 22:06:16 +0000

    DontDestroyOnLoad will remove anything else with DontDestroyOnLoad component attached, so 2 scenes can both have a mainCamera etc, and whichever one loads first will destroy the one in the scene being switched to / reloaded.

--- Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/Helpers/DontDestroyOnLoad.cs
+++ Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/Helpers/DontDestroyOnLoad.cs
@@ -7,6 +7,13 @@ public class DontDestroyOnLoad : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         DontDestroyOnLoad (gameObject);
+		// If the same type of gameobject is present in scene already than it is destroyed useful when reloading the same scene
+		if (FindObjectsOfType(GetType()).Length > 1)
+		{
+			Destroy(gameObject);
+		}
+		transform.localPosition = Vector3.zero;
+		transform.localRotation = Quaternion.identity;
 	}
 	
 	// Update is called once per frame
